4ea1a24d2c45997c88ec47aa2eb0f52bd5672621
DS0708 <jonum12312@gmail.com>
2024-09-23 20:38:45 +0900

    config server init

--- src/main/java/com/polarbookshop/configservice/ConfigServiceApplication.java
+++ src/main/java/com/polarbookshop/configservice/ConfigServiceApplication.java
@@ -2,8 +2,10 @@ package com.polarbookshop.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer //스프링 부트 애플리케이션에서 Config server를 활성화 (자바 쪽에서는 이것이 전부)
 public class ConfigServiceApplication {
 
     public static void main(String[] args) {
